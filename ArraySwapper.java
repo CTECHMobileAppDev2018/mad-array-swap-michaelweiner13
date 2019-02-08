2ce3034a1c27f67cde85abcf33bc66cbf0c0a5a1
Michael <6993102@isd535.org>
2019-02-08 06:49:18 -0600

    Update ArraySwapper.java

--- ArraySwapper.java
+++ ArraySwapper.java
@@ -1,23 +1,28 @@
 class ArraySwapper {
-  
+
   public int[] arraySwap(int []inputArray) {
-    
+
     // Swap the two items in "inputArray" and return the output
     int[] outputArray = new int[2];
 
-    // put your code here
-    
+    // Set the first item of the outputArray to the second item of the inputArray
+    outputArray[0] = inputArray[1];
+
+    // Set the second item of the outputArray to the first item of the inputArray
+    outputArray[1] = inputArray[0];
+
+    // Return the new, swapped array
     return outputArray;
-    
+
   }
-  
+
   public static void main(String[] args) {
 
     ArraySwapper testSwapper = new ArraySwapper();
-    
+
     int[] exampleInput = {1,2};
     int[] exampleOutput = testSwapper.arraySwap(exampleInput);
-    
+
     if (exampleInput[0] != exampleOutput[1] || exampleInput[1] != exampleOutput[0]) {
       System.out.println("Swap failed");
     } else {
