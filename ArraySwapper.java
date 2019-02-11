f8d1b7861a92ba5faab14aa35e070cc373e84e46
Michael <6993102@isd535.org>
2019-02-11 15:22:35 -0600

    Update ArraySwapper.java

--- ArraySwapper.java
+++ ArraySwapper.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
-class Main {
-  
+class ArraySwapper {
+
   public void arraySwap(int []inputArray) {
     // Initalize a variable named 'temp' to store the value of inputArray[0]
     int temp;
@@ -11,13 +11,13 @@ class Main {
     inputArray[0] = inputArray[1];
     inputArray[1] = temp;
   }
-  
+
   // This method swaps any 2 items in any array
   public void arraySwapAny2(int []inputArray, int loc1, int loc2) {
-    
+
     // Initialize a variable 'temp' to help switch the values of the arrays
     int temp;
-    
+
     // First check to make sure that 'loc1' and 'loc2' are both greater than zero AND less than the array's length
     if ((loc1 > 0 && loc1 < inputArray.length) && (loc2 > 0 && loc2 < inputArray.length)) {
       temp = inputArray[loc1]; // Store the first value to be switched into 'temp'
@@ -29,18 +29,18 @@ class Main {
   public static void main(String[] args) {
 
     Main testSwapper = new Main();
-    
+
     // ***** Test the first swap method *******
     System.out.println("*** Trying first method ***");
     int[] exampleInput = {1, 2};
     int[] exampleOutput = {2, 1};
-    System.out.println("exampleInput before: " + Arrays.toString(exampleInput));    
-    System.out.println("exampleOutput: " + Arrays.toString(exampleOutput));    
-    
+    System.out.println("exampleInput before: " + Arrays.toString(exampleInput));
+    System.out.println("exampleOutput: " + Arrays.toString(exampleOutput));
+
     // This calls the first swap method
     testSwapper.arraySwap(exampleInput);
-    System.out.println("exampleInput after: " + Arrays.toString(exampleInput));    
-    
+    System.out.println("exampleInput after: " + Arrays.toString(exampleInput));
+
     if (java.util.Arrays.equals(exampleInput, exampleOutput)) {
       System.out.println("arraySwap succeeded");
     } else {
@@ -51,13 +51,13 @@ class Main {
     System.out.println("\n*** Trying second method ***");
     exampleInput = new int[]{1, 22, 333, 4444};
     exampleOutput = new int[]{1, 333, 22, 4444};
-    System.out.println("exampleInput before: " + Arrays.toString(exampleInput));    
-    System.out.println("exampleOutput: " + Arrays.toString(exampleOutput));    
-    
+    System.out.println("exampleInput before: " + Arrays.toString(exampleInput));
+    System.out.println("exampleOutput: " + Arrays.toString(exampleOutput));
+
     // This calls the second swap method
     testSwapper.arraySwapAny2(exampleInput, 1, 2);
-    System.out.println("exampleInput after: " + Arrays.toString(exampleInput));    
-    
+    System.out.println("exampleInput after: " + Arrays.toString(exampleInput));
+
     if (java.util.Arrays.equals(exampleInput, exampleOutput)) {
       System.out.println("arraySwapAny2 succeeded");
     } else {
